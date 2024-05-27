5509b3d1ef564c1276a29058f576ff4613cd8713
greverXD <grevermain@gmail.com>
2024-05-27 22:21:52 +0300

    1 module - done

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,15 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str === "") {
+      return "";
+  }
+
+  if (str.length === 1) {
+      return str[0].toUpperCase();
+  } else {
+      return str[0].toUpperCase() + str.slice(1);
+  }
 }
+
+console.log(ucFirst("v"));
+console.log(ucFirst("vas"));
+console.log(ucFirst(""));
