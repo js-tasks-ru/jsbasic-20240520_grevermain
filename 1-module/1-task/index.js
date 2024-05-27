81bb160300b38570ec2f4edb2d3c5b7e085e8fb4
greverXD <grevermain@gmail.com>
2024-05-28 00:19:04 +0300

    add 4 task

---
+++ 1-module/1-task/index.js
@@ -0,0 +1,15 @@
+function factorial(n) {
+  if (n < 0) {
+      return undefined; 
+  }
+
+  let result = 1;
+  let i = n;
+
+  while (i > 1) {   // for(let i = n;i > 1; i--)
+      result *= i;
+      i--;
+  }
+
+  return result;
+}
---
+++ 1-module/4-task/index.js
@@ -0,0 +1,7 @@
+function checkSpam(str) {
+  if (str.includes("1XbeT now") || str.includes("free xxxxx")){
+    return true;
+  }else{
+    return false;
+  }
+}
