97e52e6992ea4c26960fecbacaac7f71cc497c21
greverXD <grevermain@gmail.com>
2024-06-04 01:17:19 +0300

    2-module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (typeof salaries[key] === 'number' && salaries[key] !== Infinity && salaries[key] !== -Infinity && !isNaN(salaries[key])) {
+      sum += salaries[key];
+    }
+  }
+  return sum;
 }
+
